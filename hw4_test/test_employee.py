3dbb052288f252a4d2fe220b1f20f5d1bf2ae01d
Diana_Obarianyk <dianaoba13@gmail.com>
2021-05-12 02:07:57 +0300

    Change the initial version of task#2 for hw4

---
+++ hw4_test/test_employee.py
@@ -0,0 +1,33 @@
+import unittest
+from unittest.mock import patch
+from employee import Employee
+import requests
+import requests_mock
+from unittest.mock import Mock, patch
+
+
+class TestForEmployee(unittest.TestCase):
+
+    def setUp(self):
+        self.classTest = Employee('Diana', 'Obarianyk', 10)
+
+    def test_email(self):
+        self.assertEqual(self.classTest.email, 'Diana.Obarianyk@email.com')
+
+    def test_fullname(self):
+        self.assertEqual(self.classTest.fullname, 'Diana Obarianyk')
+
+    def test_apply_raise(self):
+        self.classTest.apply_raise()
+        self.assertEqual(self.classTest.pay, 10)
+
+
+    @patch('requests.get')
+    def test_monthly_schedule(self, mock_get):
+        mock_get.return_value.ok = True
+        response = self.classTest.monthly_schedule('January')
+        self.assertIsNotNone(response)
+
+
+
+
